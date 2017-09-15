12d6e65441c5cc91d23eff69bd82b3c7589b60ff
kr <ktrhx4@mail.missouri.edu>
2017-09-15 14:38:51 -0700

    add app.rb functionality

--- app.rb
+++ app.rb
@@ -1,7 +1,36 @@
-require 'rubygems'
-require 'bundler/setup'
+require ('rubygems')
+require ('bundler/setup')
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/word')
 require('pry')
+
+get('/') do
+  @list = Word.all()
+  erb(:input)
+end
+
+post('/') do
+  vocab = params["vocab"]
+  definition = params["definition"]
+
+  word = Word.new(vocab, definition)
+
+  word.save()
+  @list = Word.sort
+  erb(:input)
+end
+
+get('/words/:id') do
+  @word = Word.find(params[:id])
+  erb(:output)
+end
+
+post('/words/:id') do
+  @word = Word.find(params[:id])
+  Word.delete(@word.id)
+  @list = Word.sort
+  redirect '/'
+  erb(:output)
+end
